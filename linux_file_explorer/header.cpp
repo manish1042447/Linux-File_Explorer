1f57fd9dc22c7d45faf84fb6a2a52d69354283be
manish1042447 <manish1042447@gmail.com>
2021-12-27 21:45:17 +0530

    Commiting the code with functionality of traverse the files in current and selecting the files

---
+++ linux_file_explorer/header.cpp
@@ -0,0 +1,13 @@
+#ifndef HEADER_INCLUDED
+#define HEADER_INCLUDED
+#include <bits/stdc++.h>
+#include <filesystem>
+#include <sys/stat.h>
+#include<pwd.h>
+#include <grp.h>
+#include <termios.h>
+#include <sys/ioctl.h>
+using namespace std;
+using namespace std::filesystem;
+#include "DirectoryEntry.cpp"
+#endif 
---
+++ linux_file_explorer/main.cpp
@@ -0,0 +1,7 @@
+#include "header.cpp"
+using namespace std;
+int main(){
+	path p = current_path();
+	printDir(p);
+	return 0;
+}
